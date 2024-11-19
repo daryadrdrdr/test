4e907bf93d8385d4b0bef1cb89984c2e850a8d3d
daryadrdrdr <lazycrocodile69@gmail.com>
2024-11-19 13:11:29 +1000

    test_twoUPD_testcpp_added

---
+++ test.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	long long n, m, i, j, kolichestvo;
+	cin >> n >> m;
+	vector < long long > plot, mesk;
+
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
+		{
+			long long x;
+			cin >> x;
+			plot.push_back(x);
+		}
+	cin >> kolichestvo;
+	for (i = 0; i < kolichestvo; i++)
+	{
+		long long x;
+		cin >> x;
+		mesk.push_back(x);
+	}
+	sort(plot.begin(), plot.end());
+	sort(mesk.begin(), mesk.end());
+
+	kolichestvo = 0;
+	i = plot.size() - 1;
+	j = mesk.size() - 1;
+	while ((i + 1) * (j + 1) > 0)
+	{
+		if (plot[i] >= mesk[j])
+		{
+			kolichestvo++;
+			i--;
+			j--;
+		}
+		else
+			j--;
+	}
+	cout << kolichestvo << endl;
+}
