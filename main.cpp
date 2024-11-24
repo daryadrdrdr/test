0bd3387d2f34b6f54b36a2db84441f1bc08fe427
daryadrdrdr <lazycrocodile69@gmail.com>
2024-11-24 18:06:18 +1000

    mainUPD

--- main.cpp
+++ main.cpp
@@ -1,44 +1,22 @@
-#include <iostream> 
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include <iostream>
+int i;
+int f (int n){
+     if (n==0) return 0;
+     if (n==1) return 1;
+     if (n>1) return f(n-1)+f(n-2);
+}
 
-int main()
-{
-	long long n, m, i, j, kos;
-	cin >> n >> m;
-	vector < long long > plot, mesk;
+int fac (int n){
+    if (n<1) return 1;
+    else return n*fac(n-1);
+}
 
-	for (i = 0; i < n; i++)
-		for (j = 0; j < m; j++)
-		{
-			long long x;
-			cin >> x;
-			plot.push_back(x);
-		}
-	cin >> kos;
-	for (i = 0; i < kos; i++)
-
-	{
-		long long x;
-		cin >> x;
-		mesk.push_back(x);
-	}
-	sort(plot.begin(), plot.end());
-	sort(mesk.begin(), mesk.end());
-	kos = 0;
-	i = plot.size() - 1;
-	j = mesk.size() - 1;
-	while ((i + 1) * (j + 1) > 0)
-	{
-		if (plot[i] >= mesk[j])
-		{
-			kos++;
-			i--;
-			j--;
-		}
-		else
-			j--;
-	}
-	cout << kos << endl;
+int main() {
+    int n=1;
+    std::cin >> i;
+    while (n<=i) {
+        std::cout << f(n) << ',';
+        n++;
+    }
+    return 0;
 }
