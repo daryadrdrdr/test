0b0cb59fc39afe29c2639cbff68663294148f311
daryadrdrdr <lazycrocodile69@gmail.com>
2024-11-24 19:44:22 +1000

    LAB5

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+/// Number of iteration
 int i;
+/*!
+    \brief Fibonacci series function
+    A recursive function of type int, designed to calculate Fibonacci series.
+    \param[in] n
+    The number of member, type int.
+    \return
+    Returns an int value, which is the result of calculation.
+*/
 int f (int n)
 {
      if (n==0) return 0;
      if (n==1) return 1;
      if (n>1) return f(n-1)+f(n-2);
 }
+/*!
+    \brief the main function
+	The execution of the program begins with it
+*/
+
 int main() 
 {
     int n=1;
@@ -17,3 +31,4 @@ int main()
     }
     return 0;
 }
+
